8b0dcf0fa54c31dccc0fa47c61beefd3894a2638
JannisHal <44263801+JannisHal@users.noreply.github.com>
2026-05-18 09:53:53 +0000

    Deploying to gh-pages from @ JannisHal/jannishal.github.io@ed55655648bb95d264c5a7947acee6b26a25fd13 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "JannisHal/jannishalbey.github.io",
+    "data-repo": "JannisHal/jannishal.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
